5415e1b30faccfcade6230d43ef2e9484e5074ef
Martsinkevich Artur <74377629+mrcnkwcz@users.noreply.github.com>
2024-04-18 19:14:52 +0300

    Networking 101/4: Determine the IPv4 network prefix in CIDR notation
    Fix types

--- kata/network_cidr.py
+++ kata/network_cidr.py
@@ -2,5 +2,5 @@ import ipaddress
 
 
 def network_cidr(ipv4_addr: str, net_mask: str) -> str:
-    net = ".".join(map(lambda x, y: x & y, ipv4_addr.split("."), net_mask.split(".")))
-    return ipaddress.IPv4Network(net).exploded
+    net = ".".join(map(lambda x, y: str(int(x) & int(y)), ipv4_addr.split("."), net_mask.split(".")))
+    return ipaddress.IPv4Network(net + "/" + net_mask).exploded
